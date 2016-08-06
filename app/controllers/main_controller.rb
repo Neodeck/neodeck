ca1d3fe96d63473c196c1d35e0e434bc8f7c34c0
TJ Horner <me@tjhorner.com>
2016-08-05 19:50:11 -0700

    FAQ Open Graph

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,4 +10,16 @@ class MainController < ApplicationController
       :value => "Make your own Cards Against Humanity decks in realtime with friends, then play with them in a game of CAH!"
     })
   end
+
+  def faq
+    @open_graph.push({
+      :prop => "title",
+      :value => "CAH Creator FAQ"
+    })
+
+    @open_graph.push({
+      :prop => "description",
+      :value => "You should really read this before asking a question."
+    })
+  end
 end
